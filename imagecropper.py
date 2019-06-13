9d015665632e9047ba6a3a221e9bf3ae0f45c3f6
Sinjin Jang <sinjin@stronghold.kr>
2019-06-13 16:18:35 +0900

    code formatting

--- imagecropper.py
+++ imagecropper.py
@@ -17,12 +17,12 @@ def draw_box(img, p0, p1):
 def save_box(img, p0, p1):
     now = datetime.datetime.now()
     filename = now.strftime('%Y-%m-%d_%H-%M-%S')
-    
+
     x0 = min(p0[0], p1[0])
     y0 = min(p0[1], p1[1])
     x1 = max(p0[0], p1[0])
     y1 = max(p0[1], p1[1])
-    
+
     img_boxed = img[y0:y1, x0:x1]
     cv2.imwrite(filename + '.png', img_boxed)
 
@@ -55,12 +55,14 @@ def main():
 
     while True:
         k = cv2.waitKey(100) & 0xFF
-        if k == 27 or cv2.getWindowProperty('image', cv2.WND_PROP_VISIBLE) < 1:  # wait for ESC key to exit
-           break
-        elif k == ord('s'):  # wait for 's' key to save
+        if k == 27 or cv2.getWindowProperty('image', cv2.WND_PROP_VISIBLE) < 1:
+            # wait for ESC key to exit
+            break
+        elif k == ord('s'):
+            # wait for 's' key to save
             if p0 is not None and p1 is not None:
                 save_box(img, p0, p1)
-    
+
     cv2.destroyAllWindows()
 
 
