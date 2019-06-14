b391343c00b5e6f02b361ebe508eed2f4f4a722e
Sinjin Jang <sinjin@stronghold.kr>
2019-06-14 13:33:44 +0900

    Added feature to resize image

--- postjob.py
+++ postjob.py
@@ -6,7 +6,7 @@ import cv2
 import numpy as np
 
 
-def do_main(dir_in, dir_out):
+def do_main(dir_in, dir_out, size):
     files = [{'src': os.path.join(dir_in, file), 'dst': os.path.join(dir_out, file)}
              for file in os.listdir(dir_in) if os.path.isfile(os.path.join(dir_in, file))]
 
@@ -15,12 +15,17 @@ def do_main(dir_in, dir_out):
         if img is None:
             continue
 
+        # make image as squared
         h, w, c = img.shape
         max_len = max(h, w)
         img_squared = np.zeros((max_len, max_len, c), np.uint8)
         img_squared[0:h, 0:w] = img
 
-        cv2.imwrite(file['dst'], img_squared)
+        # image resize
+        img_resized = cv2.resize(img_squared, dsize=(size, size), interpolation=cv2.INTER_AREA)
+
+        # save image
+        cv2.imwrite(file['dst'], img_resized)
 
 
 if __name__ == '__main__':
@@ -30,6 +35,8 @@ if __name__ == '__main__':
                         default='img-cropped', help='specify directory to load cropped images')
     parser.add_argument('-o', '--output-directory', type=str,
                         default='img-squared', help='specify directory to save squared images')
+    parser.add_argument('-s', '--size', type=int,
+                        default=128, help='specify the size of one side of squared images for resizing')
     args = parser.parse_args()
 
     # check and initialize
@@ -39,4 +46,4 @@ if __name__ == '__main__':
         os.mkdir(args.output_directory)
 
     # run main job for image cropping
-    do_main(args.input_directory, args.output_directory)
+    do_main(args.input_directory, args.output_directory, args.size)
