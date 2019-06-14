9bcdb24737132b22d76dedfe778790bdec9ebb50
Sinjin Jang <sinjin@stronghold.kr>
2019-06-14 11:51:21 +0900

    Rename default input/output directory

--- imagecropper.py
+++ imagecropper.py
@@ -18,7 +18,7 @@ def draw_box(_img, _p0, _p1):
 
 
 # Save the boxed area as an image
-def save_box(_img, _p0, _p1, _dir_save):
+def save_box(_img, _p0, _p1, _dir_out):
     now = datetime.datetime.now()
     filename = now.strftime('%Y-%m-%d_%H-%M-%S')
 
@@ -28,7 +28,7 @@ def save_box(_img, _p0, _p1, _dir_save):
     y1 = max(_p0[1], _p1[1])
 
     img_boxed = img[y0:y1, x0:x1]
-    cv2.imwrite(os.path.join(_dir_save, filename + '.png'), img_boxed)
+    cv2.imwrite(os.path.join(_dir_out, filename + '.png'), img_boxed)
 
     print('saved image x0:{0}, y0:{1}, x1:{2}, y1:{3}'.format(x0, y0, x1, y1))
 
@@ -50,11 +50,11 @@ def select_box(event, x, y, flags, param):
         draw_box(img, p0, (x, y))
 
 
-def do_main(dir_load, dir_save):
+def do_main(dir_in, dir_out):
     global img, p0, p1
 
-    files = [os.path.join(dir_load, file) for file in os.listdir(dir_load)
-             if os.path.isfile(os.path.join(dir_load, file))]
+    files = [os.path.join(dir_in, file) for file in os.listdir(dir_in)
+             if os.path.isfile(os.path.join(dir_in, file))]
 
     for file in files:
         img = cv2.imread(file, cv2.IMREAD_COLOR)
@@ -70,7 +70,7 @@ def do_main(dir_load, dir_save):
             elif k == ord('s'):
                 # wait for 's' key to save boxed image
                 if p0 is not None and p1 is not None:
-                    save_box(img, p0, p1, dir_save)
+                    save_box(img, p0, p1, dir_out)
                     cv2.imshow('image', img)
                     p0 = p1 = None
             elif k == ord('n'):
@@ -83,17 +83,17 @@ def do_main(dir_load, dir_save):
 if __name__ == '__main__':
     # parsing arguments
     parser = argparse.ArgumentParser(description='Command line argument')
-    parser.add_argument('-l', '--load-directory', type=str,
-                        default='load-img', help='specify directory to load images')
-    parser.add_argument('-s', '--save-directory', type=str,
-                        default='save-img', help='specify directory to save images')
+    parser.add_argument('-i', '--input-directory', type=str,
+                        default='img-raw', help='specify directory to load raw images')
+    parser.add_argument('-o', '--output-directory', type=str,
+                        default='img-cropped', help='specify directory to save cropped images')
     args = parser.parse_args()
 
     # check and initialize
-    if not os.path.isdir(args.load_directory):
+    if not os.path.isdir(args.input_directory):
         sys.exit('The input directory is not exist!!!')
-    if not os.path.isdir(args.save_directory):
-        os.mkdir(args.save_directory)
+    if not os.path.isdir(args.output_directory):
+        os.mkdir(args.output_directory)
 
     # run main job for image cropping
-    do_main(args.load_directory, args.save_directory)
+    do_main(args.input_directory, args.output_directory)
