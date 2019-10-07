24e96f498f81ec124135a283bb4f3a4ee69d8b8b
Sinjin Jang <sinjin@stronghold.kr>
2019-10-07 16:16:38 +0900

    Added feature to crop image as squared to imagecropper.py

--- imagecropper.py
+++ imagecropper.py
@@ -5,22 +5,57 @@ import sys
 
 import cv2
 
+
 img = None
 p0 = None
 p1 = None
 
+opt_squared = False
+
+
+# make a rectangle as squared
+def make_squared(_p0, _p1):
+    # Nothing to do if two points are same.
+    if _p0 == _p1:
+        return _p0, _p1
+
+    long_side = max(abs(_p1[0] - _p0[0]), abs(_p1[1] - _p0[1]))
+
+    if _p1[0] > _p0[0]:
+        p1_x = _p0[0] + long_side
+    else:
+        p1_x = _p0[0] - long_side
+
+    if _p1[1] > _p0[1]:
+        p1_y = _p0[1] + long_side
+    else:
+        p1_y = _p0[1] - long_side
+
+    return p0, (p1_x, p1_y)
+
 
 # draw box which selected by mouse dragging
 def draw_box(_img, _p0, _p1):
+    global opt_squared
+
     boxed = _img.copy()
+
+    if opt_squared:
+        _p0, _p1 = make_squared(_p0, _p1)
+
     boxed = cv2.rectangle(boxed, _p0, _p1, (0, 255, 0), 2)
     cv2.imshow('image', boxed)
 
 
 # Save the boxed area as an image
 def save_box(_img, _p0, _p1, _dir_out):
+    global opt_squared
+
     now = datetime.datetime.now()
     filename = now.strftime('%Y-%m-%d_%H-%M-%S')
+
+    if opt_squared:
+        _p0, _p1 = make_squared(_p0, _p1)
 
     x0 = min(_p0[0], _p1[0])
     y0 = min(_p0[1], _p1[1])
@@ -96,10 +131,12 @@ def do_main(dir_in, dir_out):
 if __name__ == '__main__':
     # parsing arguments
     parser = argparse.ArgumentParser(description='Command line argument')
-    parser.add_argument('-i', '--input-directory', type=str,
-                        default='img-raw', help='specify directory to load raw images')
-    parser.add_argument('-o', '--output-directory', type=str,
-                        default='img-cropped', help='specify directory to save cropped images')
+    parser.add_argument('-i', '--input-directory', type=str, default='img-raw',
+                        help='specify directory to load raw images')
+    parser.add_argument('-o', '--output-directory', type=str, default='img-cropped',
+                        help='specify directory to save cropped images')
+    parser.add_argument('-s', '--squared', action='store_true',
+                        help='to make cropped images as squared')
     args = parser.parse_args()
 
     # check and initialize
@@ -107,6 +144,8 @@ if __name__ == '__main__':
         sys.exit('The input directory is not exist!!!')
     if not os.path.isdir(args.output_directory):
         os.mkdir(args.output_directory)
+
+    opt_squared = args.squared
 
     # run main job for image cropping
     do_main(args.input_directory, args.output_directory)
